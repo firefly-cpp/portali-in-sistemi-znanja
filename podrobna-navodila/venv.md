11ef8882a8352eae13dfd850201ab44d7bbbe891
firefly-cpp <iztok@iztok-jr-fister.eu>
2022-09-12 13:39:54 +0200

    Dodatni primer

---
+++ podrobna-navodila/venv.md
@@ -0,0 +1,13 @@
+# Navidezna okolja za Python
+
+## Ustvarimo novo okolje
+
+```bash
+python -m venv okolje
+```
+ 
+## Aktiviramo novo okolje
+
+```bash
+source okolje/bin/activate
+```
---
+++ priprava-podatkov/one-hot-encoding/primer1.py
@@ -0,0 +1,7 @@
+from sklearn.preprocessing import OrdinalEncoder
+from numpy import asarray
+
+sport = asarray([['plavanje'], ['kolesarjenje'], ['tek']])
+encoder = OrdinalEncoder()
+e_sport = encoder.fit_transform(sport)
+print(e_sport)
