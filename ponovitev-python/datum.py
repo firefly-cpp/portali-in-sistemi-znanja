278aa092303f079bbcc22ff80cd5e6b383a7e927
firefly-cpp <iztok@iztok-jr-fister.eu>
2022-10-12 14:13:58 +0200

    Dodatni primeri

---
+++ ponovitev-python/datum.py
@@ -0,0 +1,11 @@
+import datetime
+
+cas = datetime.datetime.now()
+
+print("Trenutni cas: ", cas) 
+
+print("Trenutni mesec: ", cas.strftime("%B"))
+
+print ("Danasnji dan: ", cas.strftime("%A"))
+
+# referenca za ostale kode: https://www.w3schools.com/python/python_datetime.asp 
---
+++ ponovitev-python/razred.py
@@ -0,0 +1,29 @@
+class Oseba:
+  def __init__(self, ime, starost):
+    self.ime = ime
+    self.starost = starost
+
+  def izpisi_starost(self):
+    print("Starost je: " + str(self.starost))
+    
+  def __str__(self):
+    return f"{self.ime}"
+
+o1 = Oseba("Janez", 20)
+
+print ("Ime: ", o1.ime)
+
+print ("Starost: ", o1.starost)
+
+print (o1)
+
+o1.starost = 25
+
+print ("Starost: ", o1.starost)
+
+class Sportnik(Oseba):
+    pass
+
+s1 = Sportnik("Joze", 37)
+
+s1.izpisi_starost()
