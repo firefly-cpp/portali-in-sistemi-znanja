acd7c68d8284aed04db09887a15746a094da0c01
firefly-cpp <iztok@iztok-jr-fister.eu>
2022-12-07 11:30:05 +0100

    Dodatni primeri

---
+++ algoritmi-po-vzorih-iz-narave/optimizacija-hiperparametrov/preslikava.py
@@ -0,0 +1,22 @@
+# povzeto po: https://github.com/NiaOrg/NiaPy-examples/tree/master/optimize_KNN_parameters
+def get_hyperparameters(x):
+    """Get hyperparameters for solution `x`."""
+    algorithms = ('ball_tree', 'kd_tree', 'brute')
+    n_neighbors = int(5 + x[0] * 10)
+    weights = 'uniform' if x[1] < 0.5 else 'distance'
+    algorithm = algorithms[int(x[2] * 2)]
+    leaf_size = int(10 + x[3] * 40)
+
+    params =  {
+        'n_neighbors': n_neighbors,
+        'weights': weights,
+        'algorithm': algorithm,
+        'leaf_size': leaf_size
+    }
+    return params
+
+sol = [0.4, 0.22, 0.70, 0.79]
+
+parametri = get_hyperparameters(sol)
+
+print (parametri)
---
+++ algoritmi-po-vzorih-iz-narave/optimizacija-hiperparametrov/run_fa.py
@@ -0,0 +1,14 @@
+# povzeto po: https://github.com/firefly-cpp/FireflyAlgorithm
+
+# namestitev paketa: pip install fireflyalgorithm
+
+import numpy as np
+from fireflyalgorithm import FireflyAlgorithm
+
+def sphere(x):
+    return np.sum(x ** 2)
+
+FA = FireflyAlgorithm()
+best = FA.run(function=sphere, dim=10, lb=-5, ub=5, max_evals=30000)
+
+print(best)
