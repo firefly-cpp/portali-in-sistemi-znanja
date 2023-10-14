61addb492351a7dc837862074a43cf41736efc0e
Iztok Fister Jr <iztok@iztok-jr-fister.eu>
2023-10-14 19:00:48 +0200

    Update niaarm_rudarjenje.py

--- rudarjenje-asociativnih-pravil/niaarm_rudarjenje.py
+++ rudarjenje-asociativnih-pravil/niaarm_rudarjenje.py
@@ -10,7 +10,7 @@ algo = DifferentialEvolution(
     population_size=50, differential_weight=0.5, crossover_probability=0.9)
 metrics = ('support', 'confidence')
 
-rules, run_time = get_rules(data, algo, metrics, max_iters=30, logging=True)
+rules, run_time = get_rules(data, algo, metrics, max_iters=100, logging=True)
 
 print(rules)
 print(f'Run Time: {run_time}')
