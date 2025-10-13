c8901af7e7b65706a903f179308e485acc67774a
Iztok Fister, Jr. <iztok@iztok-jr-fister.eu>
2025-10-13 12:31:42 +0200

    Fix pandas json

--- priprava-podatkov/pandas/pandas_nalozi_json.py
+++ priprava-podatkov/pandas/pandas_nalozi_json.py
@@ -5,6 +5,6 @@ path = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 
     '..', '..', 'podatkovne-zbirke', 'plant-monitoring.json'
 )
-df = pd.read_csv(path)
+df = pd.read_json(path)
 
 print(df.to_string())
